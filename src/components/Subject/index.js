8223bfd7d083c4eef473c3a4de4c3be42d9bbead
alexsinfarosa <as898@cornell.edu>
2017-09-13 15:28:56 +0200

    updates

--- src/components/Subject/index.js
+++ src/components/Subject/index.js
@@ -39,20 +39,21 @@ export default class Specie extends Component {
     });
 
     const pest = toJS(specie);
+    const isPest = Object.keys(pest).length !== 0;
 
     return (
       <div style={{ marginBottom: "2rem" }}>
         <label>Pest:</label>
         <Cascader
+          placeholder="Select Pest"
           autoFocus
           expandTrigger="hover"
           allowClear={false}
           size="large"
           options={options}
           onChange={this.handleChange}
-          defaultValue={[pest.subgroup, pest.informalName]}
+          defaultValue={isPest ? [pest.subgroup, pest.informalName] : null}
           style={{ width: 200 }}
-          placeholder="Select Pest"
         />
       </div>
     );
